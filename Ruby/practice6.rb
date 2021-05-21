eab4f5f369c750b3d7820577b8528dfddfdc0e32
nagaoka1166 <Ryuu11660408@gmail.com>
2021-05-21 18:27:58 +0900

    0521の学習記録

--- Ruby/practice6.rb
+++ Ruby/practice6.rb
@@ -9,26 +9,43 @@ def area(x)
  puts x * x
 end
 area(3)
-# #問３
-# def dice(1..6)
-
-# end
-# dice.sample
+#問３
+def dice
+[1,2,3,4,5,6].dice
+end
+dice
 #問4 
 def order(drink)
     p "#{drink}をください"
 end
 
 order("カフェラテ")
-
+order("モカ")
 #問５
-問6
+def dice
+ [1,2,3,4,5,6].dice
+ p "もう一回"　if dice = 1
+end
+dice
+#問6
 def price(order,price)
     price
 end
 puts price(order: "カフェ", price: 400)
 puts price(order: "コーヒー", price: 300)
-
+#問7
+def price(item:, size:)
+    item = {"コーヒー" => 300, "カフェラテ"　=> 400}
+    size  = {"ショート"　=> 0, "トール" => 50, "ベンティ"　=> 100}
+   puts items[item] + sizes[size]
+end 
+puts puts price(item: "コーヒー", size: "ベンティ")
+問8def price(item:, size: "ショート")
+items = {"コーヒー" => 300, "カフェラテ" => 400}
+sizes  = {"ショート" => 0, "トール" => 50, "ベンティ" => 100}
+items[item] + sizes[size]
+end 
+puts  price(item: "コーヒー")
 
 #問9
 def order(drink)
