fd417ef2ce5c7e284e538f9c4bfabdbc5290ad91
nagaoka1166 <Ryuu11660408@gmail.com>
2021-07-08 20:29:02 +0900

    0708の学習記録

--- Ruby/repractice5.rb
+++ Ruby/repractice5.rb
@@ -12,3 +12,38 @@ puts [1.2].clear
 
 #問５
 puts ["カフェラテ,","モカ", "カプチーノ"].sample
+
+#問6
+puts ["大吉,中吉,末吉,凶"]
+
+#問7
+puts [100,50,300].sort
+
+#問8
+puts [100,50,300].sort.reverse
+#問9
+puts "cba".reverse
+#問10
+puts ["100","50","300"].join(",")
+
+#問11
+puts "100,50,300".split(",")
+
+#問12
+result = [1,2,3].map do |x|
+    x * 3
+end
+puts result
+
+#問13
+result = ["cba","zyx"].map do |x|
+ x.reverse
+end
+    puts result
+
+#問14
+name = ["aya", "achi", "tama"].map do |x|
+    x.downcase
+end
+    name = name.sort
+    puts name
